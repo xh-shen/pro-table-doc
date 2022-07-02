65f763ce455aef0b4997bfe72d7d1e36a65519c3
shen-web <9509394@qq.com>
2022-07-02 15:28:09 +0800

    添加licenseKey

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,11 +2,11 @@
  * @Author: shen
  * @Date: 2022-05-09 08:25:29
  * @LastEditors: shen
- * @LastEditTime: 2022-05-11 16:30:58
+ * @LastEditTime: 2022-07-02 09:27:15
  * @Description:
  */
 import ElementPlus from "element-plus";
-import ProTable from "@shene/pro-table";
+import ProTable, { setLicenseKey } from "@shene/pro-table";
 // import zhCn from "element-plus/es/locale/lang/zh-cn";
 import Layout, { globals, NotFound } from "../vitepress";
 import { define } from "../utils/types";
@@ -21,12 +21,13 @@ export default define<Theme>({
   NotFound,
   Layout,
   enhanceApp: ({ app }) => {
-    app.use(ElementPlus, {
-      // locale: zhCn,
-    });
+    setLicenseKey(
+      "c9e6b0af0c28246b2708c64f6c0c757dRE9NQUlOPXd3dy5iYWlkdS5jb20sRVhQSVJZPTE2NjI3MTM4MDM2Mjc="
+    );
+    app.use(ElementPlus as any);
     app.use(ProTable);
     globals.forEach(([name, Comp]) => {
-      app.component(name, Comp);
+      app.component(name, Comp as any);
     });
   },
 });
